3f008df40df85f0ae103c33b0be3690a502cfacb
Nemilshah1999 <nemilshah1999@gmail.com>
2020-09-04 20:06:04 +0530

    commit

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react"
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 import { toast } from "react-toastify";
-import "C:/Users/dell/Desktop/React/my-app/node_modules/react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles.css";
 
 function ContactCard(props) {
